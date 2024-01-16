8a9b7b12049ade0490a0676f8844ca470c6027cb
torrseba <113491671+torrseba@users.noreply.github.com>
2024-01-16 13:07:21 -0500

    More comments on how code is run + image examples

--- BraceRootDetection2023/testingscript.py
+++ BraceRootDetection2023/testingscript.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import os
 import poly_point_isect as bot
-import canny as cn
+import detection as cn
 
 def cannyFunc(image, output_name, low_threshold, high_threshold):
     img = cv2.imread(image)
@@ -18,16 +18,15 @@ def cannyFunc(image, output_name, low_threshold, high_threshold):
 
 cannyFunc('img_flash_set/0no_flash.jpg','flash_set_output/individuals/blur7_low200_high240_0no_flash.jpg',200,240)
 
+###THESE LINES BELOW WERE MADE TO CREATE THE EDGE DETECTED IMAGES FOUND IN THE "flash_set_output" FOLDER
+
 #for low in [30,50,70,100,130,160]:
 #    for high in [200, 240,275,300,350]:
 #        cannyFunc('img_flash_set/0no_flash.jpg','flash_set_output/canny_range_blur7/'+str(low)+'_'+str(high)+'_blur_0no_flash.jpg',low,high)
 
-#houghCircleRemove('180.jpg', 'circletest08_16/canny180.jpg', 'circletest08_16/canny180Circle.jpg','circletest08_16/canny180CircleLine.jpg')
-#cn.cannyFunc('img_flash_set/0no_flash.jpg', 'img_flash_set/invert.JPG')
 
-#for file in os.listdir(os.getcwd() + '/img_flash_set'):
-#   print(file)
+###Prototypes of calling the different functions offered by the "detection.py" library
 
-    #cn.cannyFunc('img_flash_set/'+file,'flash_set_output/canny_80_200/'+file)
-    #cn.houghLinesPFunc('img_flash_set/'+file,'flash_set_output/houghlinesp/'+file)
-    #cn.houghCircleRemove('img_flash_set/'+file,'flash_set_output/houghcirclesremove/'+'canny'+file,'flash_set_output/houghcirclesremove/'+'circles'+file,'flash_set_output/houghcirclesremove/'+'final'+file)
+#cn.cannyFunc('img_flash_set/'+file,'flash_set_output/canny_80_200/'+file)
+#cn.houghLinesPFunc('img_flash_set/'+file,'flash_set_output/houghlinesp/'+file)
+#cn.houghCircleRemove('img_flash_set/'+file,'flash_set_output/houghcirclesremove/'+'canny'+file,'flash_set_output/houghcirclesremove/'+'circles'+file,'flash_set_output/houghcirclesremove/'+'final'+file)
